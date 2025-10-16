33c1be92eddd39c37e0d21cf0e5b61490365c2eb
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-10-16 15:08:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from torchlm.models import pipnet
+from torchlm.data import LandmarksWFLWConverter, Landmarks300WConverter
+import os
+
+# ~~~~~~~~~~~~~~~~~~~~
+# ~~~ Convert data ~~~
+# ~~~~~~~~~~~~~~~~~~~~
+
+# setup your path to the original downloaded dataset from official
+# 1. WFLW:
+wflw_save_dir = "./data/WFLW/converted"
+if not os.path.exists(wflw_save_dir) or not os.listdir(wflw_save_dir):
+    converter = LandmarksWFLWConverter(
+        data_dir="../../data/WFLW", save_dir=wflw_save_dir,
+        extend=0.2, rebuild=True, target_size=256, keep_aspect=False,
+        force_normalize=True, force_absolute_path=True
+    )
+    converter.convert()
+    converter.show(count=30)  # show you some converted images with landmarks for debugging
+
+# 2. 300W:
+ibug_300w_save_dir = "./data/ibug_300W/converted"
+if not os.path.exists(ibug_300w_save_dir) or not os.listdir(ibug_300w_save_dir):
+    converter = Landmarks300WConverter(
+        data_dir="../../data/ibug_300W_large_face_landmark_dataset", save_dir=ibug_300w_save_dir,
+        extend=0.2, rebuild=True, target_size=256, keep_aspect=False,
+        force_normalize=True, force_absolute_path=True
+    )
+    converter.convert()
+    converter.show(count=30)  # show you some converted images with landmarks for debugging
+
+# ~~~~~~~~~~~~~~~~~~
+# ~~~ Load model ~~~
+# ~~~~~~~~~~~~~~~~~~
+model = pipnet(backbone="resnet18", pretrained=True, num_nb=10, num_lms=98, net_stride=32,
+               input_size=256, meanface_type="wflw", backbone_pretrained=True)
+
+# ~~~~~~~~~~~~~~~~~~~
+# ~~~ Train model ~~~
+# ~~~~~~~~~~~~~~~~~~~
+freeze_backbone = True
+model.apply_freezing(backbone=freeze_backbone)
+model.apply_training(
+    annotation_path="./data/WFLW/converted/train.txt",  # or fine-tuning your custom data
+    num_epochs=10,
+    learning_rate=0.0001,
+    save_dir="./save/pipnet",
+    save_prefix="pipnet-wflw-resnet18",
+    save_interval=10,
+    logging_interval=1,
+    device="cuda",
+    coordinates_already_normalized=True,
+    batch_size=16,
+    num_workers=4,
+    shuffle=True
+)
