50418d52ffabc9b382777328734e0207311b20d2
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-10-20 12:14:35 +0300

    Update directory names

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ from torchlm.models import pipnet
 from torchlm.data import LandmarksWFLWConverter, Landmarks300WConverter
 import os
 
-dataset_cfg = \
-    {
-        'wflw':
-            {
-                'ann_dir': 'WFLW',
-                'num_lms': 98
-            },
-        '300w':
-            {
-                'ann_dir': 'ibug_300W',
-                'num_lms': 68
-            }
-    }
+num_lms = {
+    'wflw': 98,
+    '300w': 68
+}
 
 choose_ds = '300w'  # 'wflw' / '300w'
 freeze_backbone = False
@@ -25,7 +16,7 @@ freeze_backbone = False
 
 # setup your path to the original downloaded dataset from official
 # 1. WFLW:
-wflw_save_dir = "./data/WFLW/converted"
+wflw_save_dir = "./data/wflw/converted"
 if not os.path.exists(wflw_save_dir) or not os.listdir(wflw_save_dir):
     converter = LandmarksWFLWConverter(
         data_dir="../../data/WFLW", save_dir=wflw_save_dir,
@@ -36,7 +27,7 @@ if not os.path.exists(wflw_save_dir) or not os.listdir(wflw_save_dir):
     converter.show(count=30)  # show you some converted images with landmarks for debugging
 
 # 2. 300W:
-ibug_300w_save_dir = "./data/ibug_300W/converted"
+ibug_300w_save_dir = "./data/300w/converted"
 if not os.path.exists(ibug_300w_save_dir) or not os.listdir(ibug_300w_save_dir):
     converter = Landmarks300WConverter(
         data_dir="../../data/ibug_300W_large_face_landmark_dataset", save_dir=ibug_300w_save_dir,
@@ -49,7 +40,7 @@ if not os.path.exists(ibug_300w_save_dir) or not os.listdir(ibug_300w_save_dir):
 # ~~~~~~~~~~~~~~~~~~
 # ~~~ Load model ~~~
 # ~~~~~~~~~~~~~~~~~~
-model = pipnet(backbone="resnet18", pretrained=False, num_nb=10, num_lms=dataset_cfg[choose_ds]['num_lms'],
+model = pipnet(backbone="resnet18", pretrained=False, num_nb=10, num_lms=num_lms[choose_ds],
                net_stride=32, input_size=256, meanface_type=choose_ds, backbone_pretrained=True)
 
 # ~~~~~~~~~~~~~~~~~~~
@@ -57,7 +48,7 @@ model = pipnet(backbone="resnet18", pretrained=False, num_nb=10, num_lms=dataset
 # ~~~~~~~~~~~~~~~~~~~
 model.apply_freezing(backbone=freeze_backbone)
 model.apply_training(
-    annotation_path=os.path.join('./data', dataset_cfg[choose_ds]['ann_dir'], 'converted/train.txt'),
+    annotation_path=os.path.join('./data', choose_ds, 'converted/train.txt'),
     # or fine-tuning your custom data
     num_epochs=10,
     learning_rate=0.0001,
